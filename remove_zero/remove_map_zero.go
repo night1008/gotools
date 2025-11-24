ee6dd0b6b78eb6a73a9be1245320c6eeb686e00d
许斯敏 <xusimin@xmfunny.com>
2025-11-24 18:12:22 +0800

    update remove_zero

--- remove_zero/remove_map_zero.go
+++ remove_zero/remove_map_zero.go
@@ -1,5 +1,7 @@
 package removezero
 
+import "reflect"
+
 func RemoveMapZeroNumValue(m map[string]interface{}, excludeKeysMap map[string]struct{}) map[string]interface{} {
 	for k, v := range m {
 		if _, ok := excludeKeysMap[k]; ok {
@@ -9,10 +11,42 @@ func RemoveMapZeroNumValue(m map[string]interface{}, excludeKeysMap map[string]s
 		case int, int8, int16, int32, int64,
 			uint, uint8, uint16, uint32, uint64,
 			float32, float64:
-			if val == 0 || val == 0.0 {
+			// 不能使用这种方式，int != int32
+			// if val == 0 || val == 0.0 {
+			if reflect.ValueOf(val).IsZero() {
 				delete(m, k)
 			}
 		}
 	}
 	return m
+}
+
+func IsNumericZero(v interface{}) (bool, bool) {
+	switch n := v.(type) {
+	case int:
+		return true, n == 0
+	case int8:
+		return true, n == 0
+	case int16:
+		return true, n == 0
+	case int32:
+		return true, n == 0
+	case int64:
+		return true, n == 0
+	case uint:
+		return true, n == 0
+	case uint8:
+		return true, n == 0
+	case uint16:
+		return true, n == 0
+	case uint32:
+		return true, n == 0
+	case uint64:
+		return true, n == 0
+	case float32:
+		return true, n == 0
+	case float64:
+		return true, n == 0
+	}
+	return false, false
 }
